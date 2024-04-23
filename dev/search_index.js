873274b61905bc58ae6edff5b18f44faba78ec01
Documenter.jl <documenter@juliadocs.github.io>
2024-04-23 20:16:38 +0000

    build based on 69203bb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TigerLine","category":"page"},{"location":"#TigerLine","page":"Home","title":"TigerLine","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TigerLine.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TigerLine]","category":"page"}]
+}
